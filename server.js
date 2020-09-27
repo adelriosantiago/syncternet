73f72551a45d27f3953886c05e3960e58034e23d
Alejandro del Rio <adelriosantiago@gmail.com>
2020-09-26 21:31:49 -0500

    Working socket echo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http")
 const express = require("express")
 const bodyParser = require("body-parser")
+const ws = require("ws")
 const port = 3080
 
 const app = express()
@@ -8,22 +9,45 @@ app.use(express.static("static"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.get("/exampleGetScope", (req, res) => {
+  return res.json(scope)
+})
+
 // Init server
 const server = http.createServer(app)
 
 let scope = {
   word: "starting word",
-  title: "initial title",
+  /*title: "initial title",
   subject: "random subject",
   thing: "red bold",
-  bool: "false",
-  number: "4",
+  bool: false,
+  number: 4,
   data: {
     name: "John Doe",
     address: "74 Henry Road",
-  },
+  },*/
 }
 
-server.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`)
+// Set WS server
+const wsServer = new ws.Server({ noServer: true })
+wsServer.on("connection", (socket) => {
+  console.log("connection")
+
+  socket.on("message", (message) => {
+    console.log("RX message", message)
+    wsServer.clients.forEach((client) => {
+      if (client.readyState === ws.OPEN) client.send(message)
+    })
+  })
 })
+
+server
+  .listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+  })
+  .on("upgrade", (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, (socket) => {
+      wsServer.emit("connection", socket, request)
+    })
+  })
