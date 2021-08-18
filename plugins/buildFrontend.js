9a0a932a6311f71d051673c653577f81acffb21f
Alejandro del Rio Santiago (@adelriosantiago) <adelriosantiago@gmail.com>
2021-08-17 22:54:57 -0400

    Use listdirs

--- plugins/buildFrontend.js
+++ plugins/buildFrontend.js
@@ -1,18 +1,28 @@
 const fs = require("fs")
-const getDirectories = require("../vendor/getDirectories.js")
 
 const tailwindScoped = fs.readFileSync("./vendor/tailwind.min.css", { encoding: "utf8", flag: "r" })
-const plugins = getDirectories("./plugins")
 
-let frontendExport = {
-  style: tailwindScoped,
-  plugins: {},
-}
-for (const p of plugins) {
-  frontendExport.plugins[p] = {
-    html: fs.readFileSync(`./plugins/${p}/template.html`, "utf8"),
-    middleware: {},
-    script: fs.readFileSync(`./plugins/${p}/frontend.js`, "utf8"),
+const listdirs = require("listdirs")
+
+// TODO: Use async
+listdirs("./plugins", function callback(err, plugins) {
+  if (err) {
+    console.log(err) // handle errors in your preferred way.
+  } else {
+    plugins.shift()
+    plugins = plugins.map((f) => f.match(/\w+$/g)[0])
+
+    let frontendExport = {
+      style: tailwindScoped,
+      plugins: {},
+    }
+    for (const p of plugins) {
+      frontendExport.plugins[p] = {
+        html: fs.readFileSync(`./plugins/${p}/template.html`, "utf8"),
+        middleware: {},
+        script: fs.readFileSync(`./plugins/${p}/frontend.js`, "utf8"),
+      }
+      fs.writeFileSync("./exports/frontendExport.js", `module.exports = ${JSON.stringify(frontendExport)}`)
+    }
   }
-  fs.writeFileSync("./exports/frontendExport.js", `module.exports = ${JSON.stringify(frontendExport)}`)
-}
+})
